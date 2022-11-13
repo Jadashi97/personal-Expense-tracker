cd511e81fc491c3aaae28fff4dc9f89ce296bd68
jadashi97 <njadauniversal23@yahoo.com>
2022-11-13 10:17:07 -0600

    (feat): finish the add transaction component. use Context api

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AddTransaction() {
+
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  function onSubmit(e){
+    e.preventDefault()
+
+    console.log("button clicked");
+  }
+
   return (
     <div>
-      <p>Add Transaction</p>
+      <h3>Add Transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className='form-control'>
+          <label htmlFor="text">Text</label>
+          <input type="text" placeholder="Enter text...."/>
+        </div>
+        <div className='form-control'>
+          <label htmlFor="amount">Amount <br/> (negative - expense, positive - income)</label>
+          <input type="number"  placeholder="Enter amount..."/>
+        </div>
+        <button className="btn">Add Transaction</button>
+      </form>
     </div>
   )
 }
