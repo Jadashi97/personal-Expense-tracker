3e58b09c38a78f67a360cd24dafc1cfcef93e2ff
jadashi97 <njadauniversal23@yahoo.com>
2022-11-25 21:45:01 -0600

    (feat): add the paths and connect to postman

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,1 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+const express = require('express');
