7cd6a7c6e86907e47063ebf1847feeb30a7de9ef
jadashi97 <njadauniversal23@yahoo.com>
2022-11-28 19:01:57 -0600

    (feat): added the routes and controllers for the backend

---
+++ Backend/controllers/transactions.js
@@ -0,0 +1,22 @@
+
+
+// @desc Get all transactions
+// @route Get /api/v1/transactions
+// @access Public
+exports.getTransactions = (req,res, next) => {
+    res.send("Get Transactions")
+};
+
+// @desc Post transactions
+// @route post /api/v1/transactions
+// @access Public
+exports.addTransaction = (req,res, next) => {
+    res.send("Post Transactions")
+};
+
+// @desc  transactions
+// @route Get /api/v1/transactions
+// @access Public
+exports.getTransactions = (req,res, next) => {
+    res.send("Get Transactions")
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const colors = require('colors');
+const morgan = require('morgan');
+const path = require('path');
+
+
+
+dotenv.config({path: "./config/config.env"});
+
+const transactions = require('./routes/transaction');
+
+const app = express();
+
+// app.use(express.json());
+
+
+
+app.use("/api/v3/transactions", transactions);
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+    console.log(`App is running on port ${PORT}`.yellow.bold)
+})
