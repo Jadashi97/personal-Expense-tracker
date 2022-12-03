442bfe6315c4bbc4ce1829caa37cefa727a7d7ca
jadashi97 <njadauniversal23@yahoo.com>
2022-12-02 19:52:42 -0600

    (feat): passed get method. (fix): post and delete methods

--- Backend/controllers/transactions.js
+++ Backend/controllers/transactions.js
@@ -1,17 +1,54 @@
+const Transaction = require("../models/Transaction")
 
 
 // @desc Get all transactions
 // @route Get /api/v1/transactions
 // @access Public
-exports.getTransactions = (req,res, next) => {
-    res.send("Get Transactions")
+exports.getTransactions =  async (req,res, next) => {
+    try {
+        const transactions = await Transaction.find();
+        
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+
+        })
+    }
+    
 };
 
 // @desc Post transactions
 // @route post /api/v1/transactions
 // @access Public
-exports.addTransaction = (req,res, next) => {
-    res.send("Post Transactions")
+exports.addTransaction = async (req,res, next) => {
+
+    try {
+        const {text, amount} = req.body;
+
+        const trasaction = await Transaction.create(req.body);
+        
+        return res.status(201).json({
+            success:true,
+            data: trasaction
+        })
+    } catch (error) {
+        if(err){
+            return res.status(400).json({
+                success:false,
+                error: "cmon error occured"
+            })
+        }
+        
+    }
+
 };
 
 // @desc  delete transactions
--- Backend/models/Transaction.js
+++ Backend/models/Transaction.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const TransactionSchema = new mongoose.schema({
+const TransactionSchema = new mongoose.Schema({
     text: { 
         type: String,
         trim: true,
@@ -16,3 +16,5 @@ const TransactionSchema = new mongoose.schema({
         default: Date.now
     }
 })
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
