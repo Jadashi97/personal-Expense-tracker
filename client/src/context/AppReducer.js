fed50a3635c2955935fc0eca8a331c61d79bb4b4
jadashi97 <njadauniversal23@yahoo.com>
2022-12-09 19:08:14 -0600

    (feat): solve the bug not compiling both the front and backend

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,5 +1,11 @@
 function AppReducer(state, action){
     switch(action.type){
+        // case "GET_TRANSACTIONS":
+        //     return{
+        //         ...state,
+        //         loading: false,
+        //         transactions:
+        //     }
         case "DELETE_TRANSACTION":
             return{
                 ...state,
